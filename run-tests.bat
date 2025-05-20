33864a5a70df6d0c3da01b199c5eef9156b38f7e
Luis Lagunez <A20216349@alumnos.uady.mx>
2025-05-19 23:02:42 -0600

    zxc

--- run-tests.bat
+++ run-tests.bat
@@ -2,15 +2,18 @@
 cd %~dp0
 
 echo ===== INSTALANDO DEPENDENCIAS =====
-composer install
+composer install > composer_log.txt
+type composer_log.txt
 
 echo.
 echo ===== LISTADO DE ARCHIVOS DE TEST =====
-dir core\php\tests
+dir core\php\tests > listado_archivos.txt
+type listado_archivos.txt
 
 echo.
 echo ===== EJECUTANDO PRUEBAS UNITARIAS =====
-C:\xampp\php\php.exe vendor\bin\phpunit --testdox core\php\tests\MateriasManagerTest.php
+C:\xampp\php\php.exe vendor\bin\phpunit --testdox core\php\tests\MateriasManagerTest.php > resultados.txt
+type resultados.txt
 
 echo.
 echo ===== FIN DEL PROCESO =====
