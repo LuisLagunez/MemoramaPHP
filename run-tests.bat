d039f19075d8b05f7e010215e1983ed6df35127e
Luis Lagunez <A20216349@alumnos.uady.mx>
2025-05-19 23:17:10 -0600

    asd

--- run-tests.bat
+++ run-tests.bat
@@ -1,3 +1,3 @@
 echo ===== EJECUTANDO PRUEBAS UNITARIAS ===== >> resultados.txt
-php vendor\bin\phpunit --testdox core\php\tests > resultado.txt
+php vendor\bin\phpunit --testdox core\php\tests >> resultados.txt 2>&1
 echo ===== PRUEBAS EJECUTADAS ===== >> resultados.txt
