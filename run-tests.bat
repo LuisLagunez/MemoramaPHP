5edf1a0c063e614d2742854178391e69c888b30f
Luis Lagunez <A20216349@alumnos.uady.mx>
2025-05-19 22:56:01 -0600

    asd

--- run-tests.bat
+++ run-tests.bat
@@ -9,13 +9,8 @@ echo ===== LISTADO DE ARCHIVOS DE TEST =====
 dir core\php\tests
 
 echo.
-echo ===== COMPROBANDO UBICACIÓN DE PHPUNIT =====
-dir vendor\bin > contenido_vendor_bin.txt
-type contenido_vendor_bin.txt
-
-echo.
 echo ===== EJECUTANDO PRUEBAS UNITARIAS =====
-php vendor\bin\phpunit --testdox core\php\tests\MateriasManagerTest.php > resultado.txt 2>&1
+C:\xampp\php\php.exe vendor\bin\phpunit --testdox core\php\tests\MateriasManagerTest.php > resultado.txt 2>&1
 
 echo.
 echo ===== RESULTADO DE LAS PRUEBAS =====
