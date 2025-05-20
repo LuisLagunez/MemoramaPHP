e82fcba2be6debfc3395e449e51a0c8674dfee61
Luis Lagunez <A20216349@alumnos.uady.mx>
2025-05-19 23:14:19 -0600

    asd

--- run-tests.bat
+++ run-tests.bat
@@ -1,16 +1,3 @@
-@echo off
-cd %~dp0
-
-echo ===== INSTALANDO DEPENDENCIAS ===== > resultados.txt
-composer install >> resultados.txt 2>&1
-
-echo. >> resultados.txt
-echo ===== LISTADO DE ARCHIVOS DE TEST ===== >> resultados.txt
-dir core\php\tests >> resultados.txt 2>&1
-
-echo. >> resultados.txt
 echo ===== EJECUTANDO PRUEBAS UNITARIAS ===== >> resultados.txt
-C:\xampp\php\php.exe vendor\bin\phpunit --testdox core\php\tests >> resultados.txt 2>&1
-
-echo. >> resultados.txt
-echo ===== FIN DEL PROCESO ===== >> resultados.txt
+C:\xampp\php\php.exe vendor\bin\phpunit.bat --testdox core\php\tests >> resultados.txt 2>&1
+echo ===== PRUEBAS EJECUTADAS ===== >> resultados.txt
