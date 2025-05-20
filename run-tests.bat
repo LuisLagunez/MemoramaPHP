670da6fca673662b07b85587230e76d96043a93a
Luis Lagunez <A20216349@alumnos.uady.mx>
2025-05-19 22:38:38 -0600

    .bat

---
+++ run-tests.bat
@@ -0,0 +1,16 @@
+@echo off
+cd %~dp0
+
+echo ===== INSTALANDO DEPENDENCIAS =====
+composer install
+
+echo.
+echo ===== LISTADO DE ARCHIVOS DE TEST =====
+dir core\php\tests
+
+echo.
+echo ===== EJECUTANDO PRUEBAS UNITARIAS =====
+php vendor\bin\phpunit --testdox core\php\tests\MateriasManagerTest.php
+
+echo.
+echo ===== FIN DEL PROCESO =====
